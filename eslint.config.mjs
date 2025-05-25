70162990424e24dce1caaa9248e718f71e416618
Mubashir Ahmed Siddiqui <mubashirsidiki@gmail.com>
2025-05-25 20:54:20 +0500

    chore: fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,16 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: true
-});
-
-export default [
-  {
-    ignores: ["node_modules/**", ".next/**", "build/**"],
-  },
-  ...compat.extends("next/core-web-vitals"),
-  {
-    rules: {
-      "no-unused-vars": "warn"
-    }
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  extends: ["next/core-web-vitals"],
+  ignorePatterns: ["node_modules/**", ".next/**", "build/**"],
+  rules: {
+    "no-unused-vars": "warn"
   }
-];
+};
+
+export default config;
